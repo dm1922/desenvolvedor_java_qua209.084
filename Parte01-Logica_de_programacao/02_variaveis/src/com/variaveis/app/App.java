727fcce54f5f948e3b0e8f8ac0d5019310f84b85
dm1922 <lucasr.mat@gmail.com>
2026-03-13 20:59:51 -0300

    Primeiro commit.

---
+++ Parte01-Logica_de_programacao/02_variaveis/src/com/variaveis/app/App.java
@@ -0,0 +1,8 @@
+package com.variaveis.app;
+public class App {
+    public static void main(String[] args) throws Exception {
+        String nome;
+        nome = "Lucas R.";
+        System.out.println(nome);
+    }
+}
---
+++ Parte01-Logica_de_programacao/03_tipos_variaveis/src/com/tipos/app/App.java
@@ -0,0 +1,15 @@
+package com.tipos.app;
+public class App {
+    public static void main(String[] args) throws Exception {
+        // declaração de variáveis
+        String nome = "Lucas Ribeiro";
+        int idade = 23;
+        double altura = 1.70;
+
+        // saída de dados
+        System.out.println("Nome: " + nome + ".");
+        System.out.println("Idade: " + idade + ".");
+        System.out.println("Altura: " + altura + ".");
+
+    }
+} 
